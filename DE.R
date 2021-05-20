2bd3513c93b469ce595ba5908e166935564922b9
Bram <b.snoek@outlook.com>
2021-05-20 15:39:29 +0200

    Added DE merged script

--- DE.R
+++ DE.R
@@ -1,5 +1,6 @@
 library(edgeR)
 library(R.utils)
+library(ggplot2)
 
 voom_normalize <- function(counts_table, design){
   #counts <- read.delim(counts_table)
@@ -8,9 +9,9 @@ voom_normalize <- function(counts_table, design){
   d0 <- calcNormFactors(d0 , method =c("TMM"))
   cutoff <- 1
   drop <- which(apply(cpm(d0), 1, max) < cutoff)
-  d <- d0[-drop,
+  d <- d0[-drop,]
   voom(d, design = design, plot = T)
-  v <- voom(dge, design)
+  v <- voom(d, design)
   return(v)
 }
 
@@ -32,13 +33,44 @@ read_counts_for_dir <- function(directory) {
   return(counts_table)
 }
 
-thyroid_counts_table <- read_counts_for_dir('C:/Users/Bram Snoek/thyroid/')
+thyroid_counts_table <- read_counts_for_dir("/home/bram/Downloads/Thyroid/")
 
-design_tsv <- read.table(file = 'C:/Users/Bram Snoek/GenderBiasInHormonalCancersGit/design.csv', sep = '\t', header = TRUE)
+design_tsv <- read.table(file = '/home/bram/Documents/GenderBiasInHormonalCancers/design.csv', sep = '\t', header = TRUE)
 
 sample_names <- colnames(thyroid_counts_table)
 groups <- design_tsv[sample_names, ]
 design <- model.matrix(~0+groups)
 
-normalized_thyroid_counts <- voom_normalize(thyroid_counts_table, 'C:/Users/Bram Snoek/GenderBiasInHormonalCancersGit/design.csv')
+normalized_thyroid_counts <- voom_normalize(thyroid_counts_table, design)
 
+fit <- lmFit(normalized_thyroid_counts, design)
+
+contr <- makeContrasts(groupsF - groupsM, levels = colnames(coef(fit)))
+tmp <- contrasts.fit(fit, contr)
+tmp <- eBayes(tmp)
+top.table <- topTable(tmp, sort.by = "P", n = Inf)
+length(which(top.table$adj.P.Val < 0.05))
+
+gene <- "ENSG00000241859.5"
+counts_for_top_gene <- as.data.frame(normalized_thyroid_counts$E[gene,])
+
+rownames_design <- rownames(design_tsv)
+female_htseq_files <- subset(design_tsv, Condition=="F")
+male_htseq_files <- subset(design_tsv, Condition=="M")
+
+counts_for_top_gene_f <- counts_for_top_gene[rownames(female_htseq_files), ]
+counts_for_top_gene_m <- counts_for_top_gene[rownames(male_htseq_files), ]
+
+counts_for_top_gene_f <- counts_for_top_gene_f[!is.na(counts_for_top_gene_f)]
+counts_for_top_gene_m <- counts_for_top_gene_m[!is.na(counts_for_top_gene_m)]
+
+df <- as.data.frame(matrix(nrow = 34, ncol = 2))
+colnames(df) <- c("Male", "Female")
+
+df$Female <- counts_for_top_gene_f
+df$Male <- NA
+df$Male[1:length(counts_for_top_gene_m)] <- counts_for_top_gene_m
+
+
+boxplot(df, main = paste("Ensembl gene: ", gene, "\n adj.p.value: ", top.table[gene, ]$adj.P.Val, "\n logFC: ", top.table[gene, ]$logFC),
+        ylab = "Normalized gene counts (voom)", xlab = "Gender")
