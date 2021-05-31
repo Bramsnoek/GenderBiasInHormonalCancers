b510ec7cefa9579f925e96b6a5b17cbd297b3f5d
Bram Snoek <b.snoek@outlook.com>
2021-05-31 11:51:32 +0200

    Joined normalization and differential expression analysis into one file

--- DE.R
+++ DE.R
@@ -1,9 +1,10 @@
-library(edgeR)
-library(R.utils)
-library(dplyr)
-library(ggplot2)
-
 #!/usr/bin/env Rscript
+library(R.utils)
+library(edgeR)
+library(limma)
+library(ggplot2)
+library(readr)
+
 normalization <- function(counts_table, design){
   # Make DGE list of the counts_label --> merged htseq counts files
   d0 <- DGEList(counts_table)
@@ -15,7 +16,7 @@ normalization <- function(counts_table, design){
   d <- d0[-drop,]
   # use voom to normalize 
   # voom(d, design = design, plot = T) --> used to make voom plots
-  v <- voom(dge, design)
+  v <- voom(d, design)
   # return the voom data
   return(v)
 }
@@ -30,17 +31,23 @@ linear_model_de <- function(normalized_counts, design) {
   top.table <- topTable(tmp, sort.by = "P", n = Inf)
 }
 
-# args[1] is the design.csv from the preprocessing and args[2] is file with the merged htseq counst file, args[3] is the output dir
+# args[1] is the design.csv from the preprocessing and args[2] is file with the merged htseq count file, args[3] is the output dir
 args <- commandArgs(trailingOnly = TRUE)
 
-# rewrite the design to a nummeric design 
+# rewrite the design to a numeric design
 design_tsv <- read.table(file = args[1], sep = '\t', header = TRUE)
-sample_names <- colnames(args[2])
+
+merged_files <- read_csv(args[2])
+gene_row_names <- merged_files$X1
+merged_files$X1 <- NULL
+rownames(merged_files) <- gene_row_names
+
+sample_names <- colnames(merged_files)
 groups <- design_tsv[sample_names, ]
 design <- model.matrix(~0+groups)
 
 # voom DGE data
-voomDGE <- normalization(args[1], design)
+voomDGE <- normalization(merged_files, design)
 
 lm_results <- linear_model_de(voomDGE, design)
 lm_results_0_0_5 <- lm_results[lm_results$adj.P.Val < 0.05, ]
